79eb77fd378a9e526b5c92738689127fa379c68b
PalmaPedro <pmcpalma@gmail.com>
2020-06-30 16:11:23 +0200

    .

--- config/rdsCredentials.js
+++ config/rdsCredentials.js
@@ -1,6 +1,6 @@
   
 module.exports = {
-    host: "database-3.cepmzhb3xual.eu-central-1.rds.amazonaws.com",
+    host: "sensordb.cepmzhb3xual.eu-central-1.rds.amazonaws.com",
     database: "sensordb",
     user: "admin",
     password: "q1w2e3r4t5"
